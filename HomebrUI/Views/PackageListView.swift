cb561a14a49b94dd742f203b090e3923503222bf
Ryan Davis <ryan@ittybittyapps.com>
2020-12-16 15:32:53 +1100

    Simplify package list view model state

--- HomebrUI/Views/PackageListView.swift
+++ HomebrUI/Views/PackageListView.swift
@@ -2,16 +2,10 @@ import Combine
 import SwiftUI
 
 class PackageListViewModel: ObservableObject {
-  enum State: Equatable {
-    case loading
-    case loaded([Package])
-  }
-
   @Published private(set) var isRefreshing: Bool = false
-  @Published private(set) var state: State = .loading
-  @Input var query: String = ""
+  @Published private(set) var packages: [Package] = []
 
-  private let triggerLoad = PassthroughSubject<Bool, Never>()
+  @Input var query: String = ""
 
   init(repository: PackageRepository) {
     repository.refreshing
@@ -28,9 +22,8 @@ class PackageListViewModel: ObservableObject {
           package.name.localizedCaseInsensitiveContains(query)
         }
       }
-      .map(State.loaded)
       .receive(on: DispatchQueue.main)
-      .assign(to: &$state)
+      .assign(to: &$packages)
   }
 }
 
@@ -39,21 +32,16 @@ struct PackageListView: View {
 
   var body: some View {
     VStack {
-      switch viewModel.state {
-      case .loading:
-        EmptyView()
-      case .loaded(let packages):
-        TextField("Filter", text: $viewModel.query)
-          .textFieldStyle(RoundedBorderTextFieldStyle())
-          .padding([.leading, .trailing], 8)
-        List(packages) { package in
-          NavigationLink(destination: PackageDetailView(package: package)) {
-            HStack {
-              Text(package.name)
-              Spacer()
-              Text(package.version)
-                .foregroundColor(.secondary)
-            }
+      TextField("Filter", text: $viewModel.query)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .padding([.leading, .trailing], 8)
+      List(viewModel.packages) { package in
+        NavigationLink(destination: PackageDetailView(package: package)) {
+          HStack {
+            Text(package.name)
+            Spacer()
+            Text(package.version)
+              .foregroundColor(.secondary)
           }
         }
       }
