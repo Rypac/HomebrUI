5b3cda5a170cb0b2f911a817dc5e44a5506b628c
Ryan Davis <ryan@ittybittyapps.com>
2020-12-17 09:48:50 +1100

    Add support for task cancellation

---
+++ HomebrUI/Homebrew/AsyncOperation.swift
@@ -0,0 +1,8 @@
+//
+//  AsyncOperation.swift
+//  HomebrUI
+//
+//  Created by Ryan Davis on 17/12/20.
+//
+
+import Foundation
--- HomebrUI/Homebrew/Process+Combine.swift
+++ HomebrUI/Homebrew/Process+Combine.swift
@@ -1,7 +1,12 @@
 import Combine
 import Foundation
 
-struct ProcessResult {
+struct ProcessHandle: Identifiable {
+  let id: Int
+  let cancel: () -> Void
+}
+
+struct ProcessResult: Equatable {
   let status: Int
   let standardOutput: Data
   let standardError: Data
@@ -16,31 +21,26 @@ extension Process {
   ) -> AnyPublisher<ProcessResult, Error> {
     Deferred {
       Future { completion in
-        queue.async {
-          do {
-            let result = try run(for: url, arguments: arguments, qualityOfService: qualityOfService)
+        do {
+          _ = try run(for: url, arguments: arguments, qualityOfService: qualityOfService) { result in
             completion(.success(result))
-          } catch {
-            completion(.failure(error))
           }
+        } catch {
+          completion(.failure(error))
         }
       }
     }
     .eraseToAnyPublisher()
   }
+}
 
-  private static let outputQueue: OperationQueue = {
-    let queue = OperationQueue()
-    queue.name = String("output")
-    queue.maxConcurrentOperationCount = OperationQueue.defaultMaxConcurrentOperationCount
-    return queue
-  }()
-
+extension Process {
   static func run(
     for url: URL,
     arguments: [String] = [],
-    qualityOfService: QualityOfService = .default
-  ) throws -> ProcessResult {
+    qualityOfService: QualityOfService = .default,
+    handler: @escaping (ProcessResult) -> Void
+  ) throws -> ProcessHandle {
     let task = Process()
     task.executableURL = url
     task.arguments = arguments
@@ -57,23 +57,38 @@ extension Process {
     var outputData = Data()
     var errorData = Data()
 
-    outputQueue.addOperation {
+    processQueue.addOperation {
       outputPipe.read(into: &outputData)
     }
-    outputQueue.addOperation {
+    processQueue.addOperation {
       errorPipe.read(into: &errorData)
     }
+    processQueue.addOperation {
+      task.waitUntilExit()
+    }
 
-    outputQueue.waitUntilAllOperationsAreFinished()
+    processQueue.addBarrierBlock {
+      handler(
+        ProcessResult(
+          status: Int(task.terminationStatus),
+          standardOutput: outputData,
+          standardError: errorData
+        )
+      )
+    }
 
-    task.waitUntilExit()
-
-    return ProcessResult(
-      status: Int(task.terminationStatus),
-      standardOutput: outputData,
-      standardError: errorData
+    return ProcessHandle(
+      id: Int(task.processIdentifier),
+      cancel: task.terminate
     )
   }
+
+  private static let processQueue: OperationQueue = {
+    let queue = OperationQueue()
+    queue.name = String("homebrew_process")
+    queue.maxConcurrentOperationCount = OperationQueue.defaultMaxConcurrentOperationCount
+    return queue
+  }()
 }
 
 private extension Pipe {
